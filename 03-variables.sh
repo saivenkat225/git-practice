3ac719eaa4edf191c67ff10a5ac68362d1c175b5
saivenkat <saivenkat225@gmail.com>
2024-09-17 00:04:53 +0530

    changes to variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,5 +3,5 @@
 PERSON1= Ramesh
 PERSON2=Suresh
 
-echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON1}:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: I'm good ${PERSON1}! Hope you are good too"
