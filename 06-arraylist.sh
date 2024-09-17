bc08100d1392f96d8832fdb1d6cee80bc4053b88
saivenkat <saivenkat225@gmail.com>
2024-09-17 08:33:12 +0530

    new variables

---
+++ 06-arraylist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#index means position which start with 0 and size here means array size which is 
+#3
+FRUITS=("APPLE", "BANANA", "GUAVA")
+
+echo "First Fruit is :: ${FRUITS[0]}"
+echo "second Fruit is :: ${FRUITS[1]}"
+echo "Third Fruit is :: ${FRUITS[2]}"
+
