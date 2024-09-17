f5e8ed7589a56a5dda10c43e7ec7b5728eb080a4
saivenkat <saivenkat225@gmail.com>
2024-09-17 08:42:00 +0530

    new variables

--- 06-arraylist.sh
+++ 06-arraylist.sh
@@ -8,4 +8,4 @@ echo "First Fruit is :: ${FRUITS[0]}"
 echo "second Fruit is :: ${FRUITS[1]}"
 echo "Third Fruit is :: ${FRUITS[2]}"
 
-echo "All Fruits are $FRUITS{@}"
+echo "All Fruits are $FRUITS[@]"
