f8139cf5dd6c72e99a45376606b9dffb6afc2075
saivenkat <saivenkat225@gmail.com>
2024-09-17 20:55:35 +0530

    new variables

--- 09-Special-vars.sh
+++ 09-Special-vars.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "All Variables pass to the script:: $*"
+echo "All Variables pass to the script:: $*" ##or we can use $@
+
+echo "Number of variables passed:: $#"
+
+echo "Name of the executing script:: $0"
+
+echo "current working directory:: $PWD"
+
+echo "Home directory of the current user:: $HOME"
+
+echo "PD of the script executing now:: $$"
