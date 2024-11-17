4bd25f425e8fab26a3fd3cb3c8c6224fec2ac405
saivenkat <saivenkat225@gmail.com>
2024-11-17 20:06:53 +0530

    add variables file

---
+++ variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your username:"
+
+read USERNAME
+
+echo "eneterd username is ${USERNAME}"
+
+echo "Please enter your password:"
+
+read PASSWORD
+
+echo "enetered password is ${PASSWORD}"
